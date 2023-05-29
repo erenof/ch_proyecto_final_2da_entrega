a5cd6c64188695c3e63e2b28835fd49b75dbdb52
Fernando Ereno <erenof79@gmail.com>
2023-05-29 11:14:54 -0300

    first commit

---
+++ src/main/java/com/coderhouse/proyecto_final_2da_entrega/ProyectoFinal2daEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse.proyecto_final_2da_entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinal2daEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinal2daEntregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderhouse/proyecto_final_2da_entrega/ProyectoFinal2daEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhouse.proyecto_final_2da_entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinal2daEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
